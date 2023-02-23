ee7bedb2b737d8ee4da8fa97df4710e9c52fe9f3
Elaine <e.cristina00@gmail.com>
2023-02-22 21:06:31 -0300

    Adicionando o ModelMapper ao projeto e refatorando o método salvar da Classe BookController.

--- src/main/java/com/projetolibraryapi/libraryapi/api/resource/BookController.java
+++ src/main/java/com/projetolibraryapi/libraryapi/api/resource/BookController.java
@@ -3,6 +3,7 @@ package com.projetolibraryapi.libraryapi.api.resource;
 import com.projetolibraryapi.libraryapi.api.dto.BookDTO;
 import com.projetolibraryapi.libraryapi.model.entity.Book;
 import com.projetolibraryapi.libraryapi.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,28 +12,21 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private BookService service;
+    private ModelMapper modelMapper;
 
-    public BookController(BookService service) {
+    public BookController(BookService service, ModelMapper mapper) {
         this.service = service;
+        this.modelMapper = mapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto){
-        Book entity = Book.builder()
-                .author(dto.getAuthor())
-                .title(dto.getTitle())
-                .isbn(dto.getIsbn())
-                .build();
+        Book entity = modelMapper.map(dto, Book.class);
 
         entity = service.save(entity);
 
-        return BookDTO.builder()
-                .id(entity.getId())
-                .author(entity.getAuthor())
-                .title(entity.getTitle())
-                .isbn(entity.getIsbn())
-                .build();
+        return modelMapper.map(entity, BookDTO.class);
 
     }
 }
